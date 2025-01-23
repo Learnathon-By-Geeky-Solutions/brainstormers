9fa344197721b38ee60a0c5e34027b14aca09802
Enamul Hasan <syl.enamul@gmail.com>
2025-01-23 23:38:55 +0600

    code: DotNet Core MVC CRUD Project

---
+++ EnamulHasan_CSharpLearning/DotNet/BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        
+        public int DisplayOrder { get; set; }
+        
+        public DateTime CreatedDateTime { get; set; }
+
+        // Constructor to set default values
+        public Category()
+        {
+            CreatedDateTime = DateTime.Now;
+        }
+    }
+}
