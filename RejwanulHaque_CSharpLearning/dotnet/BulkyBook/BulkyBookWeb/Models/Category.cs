42bf93b5607e272f9dd747f01e92b7b8370f3bbe
razoncdr <rejwanul7296@gmail.com>
2025-02-03 04:38:53 +0600

    Simple CRUD Project (BulkyBook) added

---
+++ RejwanulHaque_CSharpLearning/dotnet/BulkyBook/BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key]  // sets Id as primary key 
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        [DisplayName("Display Order")]
+        [Range(1, 1000, ErrorMessage ="Display Order must be between 1 and 1000!")]
+        public int DisplayOrder { get; set; }
+        public DateTime DateTime { get; set; } = DateTime.Now;
+    }
+}
