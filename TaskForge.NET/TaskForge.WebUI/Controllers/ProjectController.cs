0be485840a72cd3f651aa5b88ee7ff49426148ef
razoncdr <rejwanul7296@gmail.com>
2025-02-18 20:44:30 +0600

    feat: Project Indexcreated (without clean architecture)

---
+++ TaskForge.NET/TaskForge.WebUI/Controllers/ProjectController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using TaskForge.Infrastructure.Data;
+
+namespace TaskForge.WebUI.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Projects
+        public async Task<IActionResult> Index()
+        {
+            var projectList = await _context.Projects.ToListAsync();
+            return View(projectList);
+        }
+
+        // GET: Projects/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+
+        // POST: Books/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Author,ISBN,Price")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(book);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+    }
+}
