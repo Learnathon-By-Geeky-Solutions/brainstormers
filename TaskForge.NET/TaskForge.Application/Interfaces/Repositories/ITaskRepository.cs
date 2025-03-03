755129cbaa038360fabba7d3f5233c13a6e00163
razoncdr <rejwanul7296@gmail.com>
2025-03-03 15:21:01 +0600

    feat(Task): Added Basic Repository and Service layers for TaskItem management

---
+++ TaskForge.NET/TaskForge.Application/Interfaces/Repositories/ITaskRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskForge.Application.DTOs;
+using TaskForge.Domain.Entities;
+
+namespace TaskForge.Application.Interfaces.Repositories
+{
+    public interface ITaskRepository
+    {
+        Task<TaskItem?> GetByIdAsync(int id);
+    }
+}
---
+++ TaskForge.NET/TaskForge.Infrastructure/Repositories/TaskRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using TaskForge.Application.DTOs;
+using TaskForge.Application.Interfaces.Repositories;
+using TaskForge.Domain.Entities;
+using TaskForge.Infrastructure.Data;
+
+namespace TaskForge.Infrastructure.Repositories
+{
+    public class TaskRepository : ITaskRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TaskRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TaskItem?> GetByIdAsync(int id)
+        {
+            return await _context.TaskItems.FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+    }
+}
