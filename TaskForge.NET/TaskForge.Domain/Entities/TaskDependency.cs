e47179b548ad11271739c3e617ee808efcbd5de4
Rejwanul Haque Rajon <rejwanul7296@gmail.com>
2025-04-14 04:20:28 +0600

    feat(task): add support for task dependencies
    Introduced TaskDependency entity for self-referencing many-to-many relationship
    
    Updated TaskItem to include Dependencies and DependentOnThis collections
    
    Configured EF Core relationships and cascade behavior in DbContext
    
    Applied initial migration and database update

---
+++ TaskForge.NET/TaskForge.Domain/Entities/TaskDependency.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskForge.Domain.Entities
+{
+    public class TaskDependency
+    {
+        public int TaskId { get; set; }
+        public TaskItem Task { get; set; } = null!;
+
+        public int DependsOnTaskId { get; set; }
+        public TaskItem DependsOnTask { get; set; } = null!;
+    }
+
+}
